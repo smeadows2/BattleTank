a52629c70b8d783913d30b077e1fe4de186994c1
Sean Meadows <sean.paul.meadows@gmail.com>
2020-03-10 22:00:00 -0400

    Getting the Player Controller with C++
    In TankAIController class, found/got the player controller in similar way to getting the ai controller, logged to output, will be used for ai targeting player

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,6 +2,9 @@
 
 #include "BattleTank.h"
 #include "TankAIController.h"
+#include "Engine/World.h"
+#include "GameFramework/PlayerController.h"
+#include "GameFramework/Actor.h"
 
 void ATankAIController::BeginPlay() {
 	Super::BeginPlay();
@@ -12,9 +15,21 @@ void ATankAIController::BeginPlay() {
 	else {
 		UE_LOG(LogTemp, Warning, TEXT("AIController possessing: %s"), *(ControlledTank->GetName()));
 	}
-
+	auto PlayerTank = GetPlayerTank();
+	if (!PlayerTank) {
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("AIController tank %s found PlayerTank %s"), *(ControlledTank->GetName()), *(PlayerTank->GetName()));
+	}
 }
 
 ATank* ATankAIController::GetControlledTank() const {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const {
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+	return Cast<ATank>(PlayerPawn);
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,7 +14,8 @@ UCLASS()
 class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
-public:
+private:
 	ATank* GetControlledTank() const;
 	virtual void BeginPlay() override;
+	ATank* GetPlayerTank() const;
 };
